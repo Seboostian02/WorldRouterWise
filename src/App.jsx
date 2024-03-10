aa088d0555e87622fab04330098045f35005e3e8
Seboostian02 <antique_sebi@yahoo.ro>
2024-03-10 16:10:16 +0200

    InitCommit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,15 @@
+import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
+}
+
+export default AppLayout;
